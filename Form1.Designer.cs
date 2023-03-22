b06929cbcf1d846b18d8bd10caca995f98b413e8
HASAN MAHMUD <123263804+MUHENDIShasanMAHMUD@users.noreply.github.com>
2023-03-22 17:33:06 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace Calculater_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.txt_n1 = new System.Windows.Forms.TextBox();
+            this.txt_n2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(264, 215);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(214, 215);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Info;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(164, 215);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(42, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "/";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Info;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(114, 215);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(42, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "*";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(80, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Birinci sayı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Location = new System.Drawing.Point(252, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "İknci sayı :";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(152, 297);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 37);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SONUÇ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_result.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_result.Location = new System.Drawing.Point(102, 349);
+            this.lbl_result.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(234, 34);
+            this.lbl_result.TabIndex = 7;
+            this.lbl_result.Text = "00";
+            this.lbl_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_result.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txt_n1
+            // 
+            this.txt_n1.Location = new System.Drawing.Point(102, 96);
+            this.txt_n1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_n1.Name = "txt_n1";
+            this.txt_n1.Size = new System.Drawing.Size(60, 30);
+            this.txt_n1.TabIndex = 8;
+            this.txt_n1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_n2
+            // 
+            this.txt_n2.Location = new System.Drawing.Point(264, 96);
+            this.txt_n2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_n2.Name = "txt_n2";
+            this.txt_n2.Size = new System.Drawing.Size(60, 30);
+            this.txt_n2.TabIndex = 9;
+            this.txt_n2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(444, 461);
+            this.Controls.Add(this.txt_n2);
+            this.Controls.Add(this.txt_n1);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Calculater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.TextBox txt_n1;
+        private System.Windows.Forms.TextBox txt_n2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculater_2
+{
+    public partial class Form1 : Form
+    {
+        double n1;
+        double n2;
+        double result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txt_n1.Text);//Convert the entered string(text) to a double and dive the value to "n1"
+            n2 = Convert.ToDouble(txt_n2.Text);//Convert the entered string(text) to a double and dive the value to "n2"
+            result = n1 - n2;//Perform the operation and give it to "result"
+            lbl_result.Text = Convert.ToString(result);//convert the value from a double to string adf write it in the output
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txt_n1.Text);//Convert the entered string(text) to a double and dive the value to "n1"
+            n2 = Convert.ToDouble(txt_n2.Text);//Convert the entered string(text) to a double and dive the value to "n2"
+            result = n1 * n2;//Perform the operation and give it to "result"
+            lbl_result.Text = Convert.ToString(result);//convert the value from a double to string adf write it in the output
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txt_n1.Text); //Convert the entered string(text) to a double and dive the value to "n1"
+            n2 = Convert.ToDouble(txt_n2.Text); //Convert the entered string(text) to a double and dive the value to "n2"
+            result = n1 + n2; //Perform the operation and give it to "result"
+            lbl_result.Text = Convert.ToString(result); //convert the value from a double to string adf write it in the output
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            n1 = Convert.ToDouble(txt_n1.Text);//Convert the entered string(text) to a double and dive the value to "n1"
+            n2 = Convert.ToDouble(txt_n2.Text);//Convert the entered string(text) to a double and dive the value to "n2"
+            result = n1 / n2;//Perform the operation and give it to "result"
+            lbl_result.Text = Convert.ToString(result);//convert the value from a double to string adf write it in the output
+        }
+
+        private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
